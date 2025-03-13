174ed6205cb1d414b7b97d90d868d2ca5fa5dc4c
YU-20-10 <108886846+YU-20-10@users.noreply.github.com>
2025-03-13 23:08:25 +0800

    part1 init

--- app.py
+++ app.py
@@ -1,17 +1,178 @@
-from fastapi import *
-from fastapi.responses import FileResponse
-app=FastAPI()
+# from fastapi import *
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, JSONResponse
+import mysql.connector
+import json
+
+app = FastAPI()
+
+
+# ******
+def database():
+    return mysql.connector.connect(
+        host="127.0.0.1", user="root", passwd="*", database="taipei_trip"
+    )
+
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
 async def index(request: Request):
-	return FileResponse("./static/index.html", media_type="text/html")
+    return FileResponse("./static/index.html", media_type="text/html")
+
+
 @app.get("/attraction/{id}", include_in_schema=False)
 async def attraction(request: Request, id: int):
-	return FileResponse("./static/attraction.html", media_type="text/html")
+    return FileResponse("./static/attraction.html", media_type="text/html")
+
+
 @app.get("/booking", include_in_schema=False)
 async def booking(request: Request):
-	return FileResponse("./static/booking.html", media_type="text/html")
+    return FileResponse("./static/booking.html", media_type="text/html")
+
+
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
-	return FileResponse("./static/thankyou.html", media_type="text/html")
+    return FileResponse("./static/thankyou.html", media_type="text/html")
+
+
+@app.get("/api/attractions", response_class=JSONResponse)
+async def attractions(page: int, keyword: str | None = None):
+    attractions_data = []
+    try:
+        database_connect = database()
+        database_connect_cursor = database_connect.cursor()
+        # 依據是否輸入捷運站名稱進行不同的MySQL查詢
+        if keyword:
+            database_connect_cursor.execute(
+                "SELECT attractions.id,attractions.name AS attractions_name,attractions.category,attractions.description,attractions.address,attractions.transport,mrt.name AS mrt_name,attractions.lat,attractions.lng,attractions.images FROM attractions INNER JOIN mrt ON attractions.mrt_id=mrt.id WHERE mrt.name=%s AND attractions.name LIKE %s ORDER BY attractions.id",
+                [keyword, f"%{keyword}%"],
+            )
+        else:
+            database_connect_cursor.execute(
+                "SELECT attractions.id,attractions.name AS attractions_name,attractions.category,attractions.description,attractions.address,attractions.transport,mrt.name AS mrt_name,attractions.lat,attractions.lng,attractions.images FROM attractions INNER JOIN mrt ON attractions.mrt_id=mrt.id ORDER BY attractions.id"
+            )
+        attractions_row_data = database_connect_cursor.fetchall()
+        for attraction in attractions_row_data:
+            input_data = {
+                "id": attraction[0],
+                "name": attraction[1],
+                "category": attraction[2],
+                "description": attraction[3],
+                "address": attraction[4],
+                "transport": attraction[5],
+                "mrt": attraction[6],
+                "lat": attraction[7],
+                "lng": attraction[8],
+                "images": json.loads(attraction[9]),
+            }
+            attractions_data.append(input_data)
+        # 處理mrt=None的資料
+        database_connect_cursor.execute(
+            "SELECT * FROM attractions WHERE mrt_id IS NULL"
+        )
+        none_mrt_data = database_connect_cursor.fetchall()
+        for data in none_mrt_data:
+            input_data = {
+                "id": data[0],
+                "name": data[1],
+                "category": data[2],
+                "description": data[3],
+                "address": data[4],
+                "transport": data[5],
+                "mrt": data[6],
+                "lat": data[7],
+                "lng": data[8],
+                "images": json.loads(data[9]),
+            }
+            attractions_data.insert(data[0] - 1, input_data)
+        database_connect_cursor.close()
+        database_connect.close()
+        # 依據得到結果return資料
+        if len(attractions_data) >= 12:
+            if page == 1 and len(attractions_data) > 12:
+                return {"nextPage": 1, "data": attractions_data[0:12]}
+            elif page > 1:
+                return {
+                    "nextPage": page,
+                    "data": attractions_data[(12 * (page - 1)) : (12 * page)],
+                }
+        elif 12 >= len(attractions_data) > 0:
+            return {"nextPage": None, "data": attractions_data}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": True, "message": str(e)})
+
+
+@app.get("/api/attractions/{attractionId}")
+async def get_attractions_by_id(attractionId: int):
+    try:
+        attractions_data = {}
+        database_connect = database()
+        database_connect_cursor = database_connect.cursor()
+        database_connect_cursor.execute(
+            "SELECT attractions.id,attractions.name AS attractions_name,attractions.category,attractions.description,attractions.address,attractions.transport,mrt.name AS mrt_name,attractions.lat,attractions.lng,attractions.images FROM attractions INNER JOIN mrt ON attractions.mrt_id=mrt.id WHERE attractions.id=%s",
+            [attractionId],
+        )
+        row_data = database_connect_cursor.fetchone()
+        if row_data:
+            attractions_data = {
+                "id": row_data[0],
+                "name": row_data[1],
+                "category": row_data[2],
+                "description": row_data[3],
+                "address": row_data[4],
+                "transport": row_data[5],
+                "mrt": row_data[6],
+                "lat": row_data[7],
+                "lng": row_data[8],
+                "images": json.loads(row_data[9]),
+            }
+        else:
+            database_connect_cursor.execute(
+                "SELECT * FROM attractions WHERE mrt_id IS NULL AND id=%s",
+                [attractionId],
+            )
+            none_mrt_data = database_connect_cursor.fetchone()
+            attractions_data = {
+                "id": none_mrt_data[0],
+                "name": none_mrt_data[1],
+                "category": none_mrt_data[2],
+                "description": none_mrt_data[3],
+                "address": none_mrt_data[4],
+                "transport": none_mrt_data[5],
+                "mrt": none_mrt_data[6],
+                "lat": none_mrt_data[7],
+                "lng": none_mrt_data[8],
+                "images": json.loads(none_mrt_data[9]),
+            }
+        return {"data": attractions_data}
+    except Exception:
+        return JSONResponse(
+            status_code=500, content={"error": "ok", "message": str(Exception)}
+        )
+
+
+@app.get("/api/mrts")
+async def mrt():
+    try:
+        mrt_data = []
+        database_connect = database()
+        database_connect_cursor = database_connect.cursor()
+        database_connect_cursor.execute(
+            "SELECT attractions.mrt_id,mrt.name FROM attractions INNER JOIN mrt ON attractions.mrt_id=mrt.id"
+        )
+        mrt_row_data = database_connect_cursor.fetchall()
+        data = {}
+        for mrt in mrt_row_data:
+            # print(mrt)
+            # print(data.get(mrt[1], None))
+            if data.get(mrt[1], None) is None:
+                data[mrt[1]] = 1
+            else:
+                data[mrt[1]] += 1
+        for key, value in sorted(data.items(), key=lambda item: item[1], reverse=True):
+            mrt_data.append(key)
+        return {"data": mrt_data}
+    except Exception:
+        return JSONResponse(
+            status_code=500, content={"error": "ok", "message": str(Exception)}
+        )
---
+++ write.py
@@ -0,0 +1,89 @@
+import json
+import mysql.connector
+
+
+# ******
+def database():
+    return mysql.connector.connect(host="127.0.0.1", user="root", passwd="*")
+
+
+attrations_data = []
+mrt_data = []
+
+with open("data/taipei-attractions.json", "r", encoding="utf-8") as row_attrations_data:
+    row_attrations_data_json = json.load(row_attrations_data)
+    for index, data in enumerate(row_attrations_data_json["result"]["results"]):
+        # 篩選圖片網址
+        images = []
+        for img in data["file"].split("https"):
+            if img and img.lower().endswith(("jpg", "png")):
+                images.append("https" + img)
+
+        # 找出mrt對應的mrt_id,若mrt為null則mrt_id也為None
+        # 處理mysql mrt資料
+        mrt_id = None
+        if len(mrt_data) > 0:
+            dataExist = False
+            for mrt in mrt_data:
+                if data["MRT"] == mrt["name"]:
+                    mrt_id = mrt["id"]
+                    dataExist = True
+                    break
+            if not dataExist and data["MRT"]:
+                mrt_id = len(mrt_data) + 1
+                mrt_data.append({"id": mrt_id, "name": data["MRT"]})
+        elif len(mrt_data) == 0 and data["MRT"]:
+            mrt_id = 1
+            mrt_data.append({"id": 1, "name": data["MRT"]})
+
+        # 處理mysql attraction資料
+        attrations_data.append(
+            {
+                "id": len(attrations_data) + 1,
+                "name": data["name"],
+                "category": data["CAT"],
+                "description": data["description"],
+                "address": data["address"],
+                "transport": data["direction"],
+                "mrt_id": mrt_id,
+                "lat": float(data["latitude"]),
+                "lng": float(data["longitude"]),
+                "images": json.dumps(images)
+            }
+        )
+
+# print(attrations_data)
+# print(mrt_data)
+
+# taipei_trip
+database_connect = database()
+database_connect_cursor = database_connect.cursor()
+# database_connect_cursor.execute("CREATE DATABASE taipei_trip")
+database_connect_cursor.execute("USE taipei_trip")
+
+# table mrt
+# database_connect_cursor.execute(
+#     "CREATE TABLE mrt(id BIGINT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(255) NOT NULL)"
+# )
+# for mrt in mrt_data:
+#     database_connect_cursor.execute(
+#         "INSERT INTO mrt (id,name) VALUES (%s,%s)", [mrt["id"], mrt["name"]]
+#     )
+# database_connect.commit()
+
+# table attrations
+# database_connect_cursor.execute(
+#     "CREATE TABLE attractions(id BIGINT PRIMARY KEY AUTO_INCREMENT,name VARCHAR(255) NOT NULL,category VARCHAR(255) NOT NULL,description TEXT NOT NULL,address VARCHAR(255) NOT NULL,transport TEXT NOT NULL,mrt_id BIGINT,lat FLOAT NOT NULL,lng FLOAT NOT NULL,images TEXT,FOREIGN KEY(mrt_id) REFERENCES mrt(id))"
+# )
+# database_connect_cursor.execute("ALTER TABLE attractions MODIFY transport TEXT")
+# for attration in attrations_data:
+#     database_connect_cursor.execute("INSERT INTO attractions (id,name,category,description,address,transport,mrt_id,lat,lng,images) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",[attration["id"],attration["name"],attration["category"],attration["description"],attration["address"],attration["transport"],attration["mrt_id"],attration["lat"],attration["lng"],attration["images"]])
+
+# database_connect_cursor.execute("SELECT * FROM attractions")
+# result = database_connect_cursor.fetchall()
+# for el in result:
+#     print(el[0])
+
+database_connect.commit()
+database_connect_cursor.close()
+database_connect.close()
